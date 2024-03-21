bc6e15a20f16d9a81d7c821fac68fec88bec02e1
Muhammad Nadeem <nadeemmalikali@hotmail.com>
2024-03-21 08:24:01 +0500

    Adding new options on Details page

--- app/Details.js
+++ app/Details.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  ScrollView,
+  Keyboard,
+  TouchableOpacity,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import {useRoute} from '@react-navigation/native';
 const TotalSlabs = (item, width) => {
-  let n1 = (width+0.5) / 1.5;
+  let n1 = (width + 0.5) / 1.5;
   let n2 = n1 - Math.floor(n1);
   let x1 = 0;
   let x2 = 0;
@@ -18,8 +27,8 @@ const TotalSlabs = (item, width) => {
   }
   let price_1 = item.i * item.slab1.price_1 * x1;
   let price_2 = item.i * item.slab1.price_2 * x2;
-  let weight_1 = item.i*item.slab1.weight_1*x1;
-  let weight_2 = item.i*item.slab1.weight_2*x2;
+  let weight_1 = item.i * item.slab1.weight_1 * x1;
+  let weight_2 = item.i * item.slab1.weight_2 * x2;
   let weight_3 = 0;
   let weight_4 = 0;
   let price_3 = 0;
@@ -42,59 +51,90 @@ const TotalSlabs = (item, width) => {
     x1: x1,
     x2: x2,
     price: price,
-    w_1:weight_1,
-    w_2:weight_2,
-    w_3:weight_3,
-    w_4:weight_4,
-    s_weight:weight_1+weight_2+weight_3+weight_4,
-    price_1_string: `${slab_1_string} x 1-6 = ${item.i}x${x1} = ${
-      item.i * x1
-    } = ${price_1} RS`,
-    price_2_string: `${slab_1_string} x 1= ${item.i}x${x2} = ${
-      item.i * x2
-    } = ${price_2} RS`,
-    price_3_string: `${slab_2_string} x 1-6 = ${item.j}x${x1} = ${
-      item.j * x1
-    } = ${price_3} RS`,
-    price_4_string: `${slab_2_string} x 1 = ${item.j}x${x2} = ${
-      item.j * x2
-    } = ${price_4} RS`,
+    w_1: weight_1,
+    w_2: weight_2,
+    w_3: weight_3,
+    w_4: weight_4,
+    s_weight: weight_1 + weight_2 + weight_3 + weight_4,
+    price_1_string: `${slab_1_string} x 1-6 = ${item.i}x${x1} = ${item.i * x1}`,
+    price_1: price_1,
+    price_2_string: `${slab_1_string} x 1= ${item.i}x${x2} = ${item.i * x2}`,
+    price_2: price_2,
+    price_3_string: `${slab_2_string} x 1-6 = ${item.j}x${x1} = ${item.j * x1}`,
+    price_3: price_3,
+    price_4_string: `${slab_2_string} x 1 = ${item.j}x${x2} = ${item.j * x2}`,
+    price_4: price_4,
   };
 };
 
-const GirdersPrice = (width,walls,girders) => {
-  const size = width+2*(walls/12);
+const GirdersPrice = (width, walls, girders) => {
+  const size = width + 2 * (walls / 12);
   let gPrice = 0;
   let weight = 0;
   let girderCombination = {};
-  if(size>=1 && size<=14.4) {
-    gPrice = size*girders*390;
-    weight = size*10.3;
-    girderCombination= {size:'4x9',price:390,totalPrice:gPrice,totaWeight:weight};
-  } else if(size>=14.5 && size<=16.9) {
-    gPrice = size*girders*420;
-    weight = size*11.5;
-    girderCombination = {size:'4x10',price:420,totalPrice:gPrice,totaWeight:weight};
-  } else if(size>=17 && size<=18.4) {
-    gPrice = size*girders*460;
-    weight = size*11.5;
-    girderCombination = {size:'4x10',price:460,totalPrice:gPrice,totaWeight:weight};
-  } else if(size>=18.5 && size<=20.9) {
-    gPrice = size*girders*520;
-    weight = size*16;
-    girderCombination = {size:'5x12',price:520,totalPrice:gPrice,totaWeight:weight};
-  } else if(size>=21 && size<=23.4) {
-    gPrice = size*girders*550;
-    weight = size*16;
-    girderCombination = {size:'5x12',price:550,totalPrice:gPrice,totaWeight:weight};
-  } else if (size>=23.5 && size<=25.4) {
-    gPrice = size*girders*660;
-    weight = size*20;
-    girderCombination = {size:'5x14',price:660,totalPrice:gPrice,totaWeight:weight};
+  if (size >= 1 && size <= 14.4) {
+    gPrice = size * girders * 390;
+    weight = size *girders* 10.3;
+    girderCombination = {
+      size: '4x9',
+      price: 390,
+      totalPrice: gPrice,
+      totaWeight: weight,
+    };
+  } else if (size >= 14.5 && size <= 16.9) {
+    gPrice = size * girders * 420;
+    weight = size *girders*11.5;
+    girderCombination = {
+      size: '4x10',
+      price: 420,
+      totalPrice: gPrice,
+      totaWeight: weight,
+    };
+  } else if (size >= 17 && size <= 18.4) {
+    gPrice = size * girders * 460;
+    weight = size * girders*11.5;
+    girderCombination = {
+      size: '4x10',
+      price: 460,
+      totalPrice: gPrice,
+      totaWeight: weight,
+    };
+  } else if (size >= 18.5 && size <= 20.9) {
+    gPrice = size * girders * 520;
+    weight = size *girders* 16;
+    girderCombination = {
+      size: '5x12',
+      price: 520,
+      totalPrice: gPrice,
+      totaWeight: weight,
+    };
+  } else if (size >= 21 && size <= 23.4) {
+    gPrice = size * girders * 550;
+    weight = size *girders* 16;
+    girderCombination = {
+      size: '5x12',
+      price: 550,
+      totalPrice: gPrice,
+      totaWeight: weight,
+    };
+  } else if (size >= 23.5 && size <= 25.4) {
+    gPrice = size * girders * 660;
+    weight = size *girders* 20;
+    girderCombination = {
+      size: '5x14',
+      price: 660,
+      totalPrice: gPrice,
+      totaWeight: weight,
+    };
   } else {
-    gPrice = size*girders*700;
-    weight = size*20;
-    girderCombination = {size:'5x14',price:700,totalPrice:gPrice,totaWeight:weight};
+    gPrice = size * girders * 700;
+    weight = size*girders* 20;
+    girderCombination = {
+      size: '5x14',
+      price: 700,
+      totalPrice: gPrice,
+      totaWeight: weight,
+    };
   }
   return girderCombination;
 };
@@ -105,66 +145,213 @@ const SlbString = slab => {
 };
 const Details = () => {
   const route = useRoute();
-  const {item, length, width,walls} = route.params;
-  const area = (length+2*(walls/12)) * (width+2*(walls/12));
+  const {item, length, width, walls} = route.params;
+  const area = (length + 2 * (walls / 12)) * (width + 2 * (walls / 12));
   const x = TotalSlabs(item, width);
-  const girdersObj = GirdersPrice(width,walls,item.cells-1);
-  const price_pp = (x.price+girdersObj.totalPrice) / area;
-  const weight_lb = x.s_weight+girdersObj.totaWeight;
+  const girdersObj = GirdersPrice(width, walls, item.cells - 1);
+  const totalPrice = x.price + girdersObj.totalPrice;
+  const weight_lb = x.s_weight + girdersObj.totaWeight;
+  const [percentage, setPercentage] = useState('');
+  const [hasting, setHasting] = useState('');
+  const [rent, setRent] = useState('');
+  const [gt, setGt] = useState('');
+  const [price_pp, setPrice_PP] = useState(gt / area);
+  const [pshow, setPshow] = useState(false);
+  useEffect(() => {
+    const a = hasting ? parseFloat(hasting) : 0;
+    const r = rent ? parseFloat(rent) : 0;
+    const b = percentage
+      ? totalPrice - (totalPrice * parseFloat(percentage)) / 100
+      : totalPrice;
+    setGt(a + b + r);
+  }, [percentage, hasting, rent]);
+  useEffect(() => {
+    setPrice_PP(gt / area);
+  }, [gt]);
+  const toggleCheckbox = () => {
+    setPshow(!pshow);
+  };
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Percentage</Text>
+        <TextInput
+          style={styles.input}
+          value={percentage}
+          onChangeText={text => setPercentage(text)}
+          keyboardType="numeric"
+          onBlur={() => Keyboard.dismiss()}
+        />
+        <Text style={styles.label}>Hasting</Text>
+        <TextInput
+          style={styles.input}
+          value={hasting}
+          onChangeText={text => setHasting(text)}
+          keyboardType="numeric"
+          onBlur={() => Keyboard.dismiss()}
+        />
+        <Text style={styles.label}>Rent</Text>
+        <TextInput
+          style={styles.input}
+          value={rent}
+          keyboardType="numeric"
+          onChangeText={text => setRent(text)}
+          onBlur={() => Keyboard.dismiss()}
+        />
+        <TouchableOpacity
+          style={styles.checkboxContainer}
+          onPress={toggleCheckbox}>
+          <Icon
+            name={pshow ? 'check-box' : 'crop-square'}
+            size={24}
+            color={pshow ? 'green' : 'black'}
+          />
+          <Text style={styles.checkboxLabel}>{pshow?'Hide Price':'Show Price'}</Text>
+        </TouchableOpacity>
+      </View>
       <View style={styles.card}>
         <Text style={styles.heading}>Details of Slabs</Text>
-        <Text style={styles.text}>{`Total Cells = ${item.cells}`}</Text>
         <Text style={styles.text}>
-          {`${item.i} Cells of ${item.slab1.length} (`}
-          <Text>{x.x1.toString()}</Text>
-          {parseFloat(x.x2)>0 && <Text>{`+ ${x.x2.toString()}`}</Text>}
-          {`)`}
+          <Text style={styles.column}>{`Total Cells`}</Text> ={' '}
+          <Text style={styles.column}>{`${item.cells}`}</Text>
+        </Text>
+        <Text style={styles.text}>
+          <Text
+            style={
+              styles.column
+            }>{`${item.i} Cells of ${item.slab1.length}`}</Text>{' '}
+          ={' '}
+          <Text style={styles.column}>{`(${x.x1.toString()}${
+            parseFloat(x.x2) > 0 ? ` + ${x.x2.toString()}` : ''
+          })`}</Text>
         </Text>
         {item.slab2 && (
           <Text style={styles.text}>
-            {`${item.j} Cells of ${item.slab2.length} (`} <Text>{x.x1.toString()}</Text>
-            {parseFloat(x.x2)>0 && <Text>{`+ ${x.x2.toString()}`}</Text>}
-            {`)`}
+            <Text
+              style={
+                styles.column
+              }>{`${item.j} Cells of ${item.slab2.length}`}</Text>{' '}
+            ={' '}
+            <Text style={styles.column}>{`(${x.x1.toString()}${
+              parseFloat(x.x2) > 0 ? ` + ${x.x2.toString()}` : ''
+            })`}</Text>
           </Text>
         )}
-        <Text style={styles.text}>{`No. Of Girders = ${item.cells - 1}`}</Text>
-        <Text style={styles.text}>{`${girdersObj.size}: ${item.cells-1}*${width+2*(walls/12)}*${girdersObj.price} = ${Math.round(girdersObj.totalPrice)} RS`}</Text>
-        <Text style={styles.text}>{`${x.price_1_string}`}</Text>
-        {x.x2 > 0 && <Text style={styles.text}>{`${x.price_2_string}`}</Text>}
-        {item.slab2 && <Text style={styles.text}>{`${x.price_3_string}`}</Text>}
-        {item.slab2 && x.x2 > 0 && (
-          <Text style={styles.text}>{`${x.price_4_string}`}</Text>
+        <Text style={styles.text}>
+          <Text style={styles.column}>{`No. Of Girders`}</Text> ={' '}
+          <Text style={styles.column}>{`${item.cells - 1}`}</Text>
+        </Text>
+        <Text style={styles.text}>
+          <Text style={styles.column}>{`${girdersObj.size}`}</Text> ={' '}
+          <Text style={styles.column}>{`${item.cells - 1}*${
+            width + 2 * (walls / 12)
+          }*${girdersObj.price}`}</Text>{' '}
+          {pshow && (
+            <Text style={styles.column}>
+              = {`${Math.round(girdersObj.totalPrice)} RS`}
+            </Text>
+          )}
+        </Text>
+        <Text style={styles.text}>
+          <Text style={styles.column}>{`${x.price_1_string}${
+            pshow ? ` = ${x.price_1} RS` : ''
+          }`}</Text>
+        </Text>
+        {x.x2 > 0 && (
+          <Text style={styles.text}>
+            <Text style={styles.column}>{`${x.price_2_string}${
+              pshow ? ` = ${x.price_2} RS` : ''
+            }`}</Text>
+          </Text>
         )}
-        <Text style={styles.text}>{`Lengthxwidth = ${length+2*(walls/12)}x${width+2*(walls/12)}`}</Text>
-        <Text style={styles.text}>{`Total Area = ${area} Sq.ft`}</Text>
+        {item.slab2 && (
+          <Text style={styles.text}>
+            <Text style={styles.column}>{`${x.price_3_string}${
+              pshow ? ` = ${x.price_3} RS` : ''
+            }`}</Text>
+          </Text>
+        )}
+        {item.slab2 && x.x2 > 0 && (
+          <Text style={styles.text}>
+            <Text style={styles.column}>{`${x.price_4_string}${
+              pshow ? ` = ${x.price_4} RS` : ''
+            }`}</Text>
+          </Text>
+        )}
         <Text style={styles.text}>
-          Total Price = <Text style={styles.important}>{`${x.price+girdersObj.totalPrice} RS`}</Text>
+          <Text style={styles.column}>{`Lengthxwidth`}</Text> ={' '}
+          <Text style={styles.column}>{`${length + 2 * (walls / 12)}x${
+            width + 2 * (walls / 12)
+          }`}</Text>
         </Text>
+        {pshow && (
+          <>
+            <Text style={styles.text}>
+              <Text style={styles.column}>{`Total Area`}</Text> ={' '}
+              <Text style={styles.column}>{`${area} Sq.ft`}</Text>
+            </Text>
+            <Text style={styles.text}>
+              <Text style={styles.column}>{`Ex.Factory`}</Text> ={' '}
+              <Text style={styles.important}>{`${totalPrice} RS`}</Text>
+            </Text>
+            {percentage ? (
+              <Text style={styles.text}>
+                <Text style={styles.column}>{percentage}%</Text> ={' '}
+                <Text style={styles.important}>{`${
+                  Math.round(totalPrice - (totalPrice * parseFloat(percentage)) / 100)
+                } RS`}</Text>
+              </Text>
+            ) : null}
+            {hasting ? (
+              <Text style={styles.text}>
+                <Text style={styles.column}>{`Hasting`}</Text> ={' '}
+                <Text style={styles.important}>{`${
+                  Math.round(area * parseFloat(hasting))
+                } RS`}</Text>
+              </Text>
+            ) : null}
+            {rent ? (
+              <Text style={styles.text}>
+                <Text style={styles.column}>{`Rent`}</Text> ={' '}
+                <Text style={styles.important}>{`${rent} RS`}</Text>
+              </Text>
+            ) : null}
+            <Text style={styles.text}>
+              <Text style={styles.column}>{`Total Price`}</Text> ={' '}
+              <Text style={styles.important}>{`${Math.round(parseFloat(gt))} RS`}</Text>
+            </Text>
+            <Text style={styles.text}>
+              <Text style={styles.column}>{`Price Per Sq.ft`}</Text> ={' '}
+              <Text style={styles.important}>{`${price_pp.toFixed(
+                2,
+              )} RS`}</Text>
+            </Text>
+          </>
+        )}
         <Text style={styles.text}>
-          Price Per Sq.ft ={' '}
-          <Text style={styles.important}>{`${price_pp.toFixed(2)} RS`}</Text>
-        </Text>
-        <Text style={styles.text}>
-          Material weight = {weight_lb} kg = {(weight_lb/1000).toFixed(2)} ton
+          <Text style={styles.column}>{`Material weight`}</Text> ={' '}
+          <Text style={styles.column}>{`${weight_lb} kg = ${(
+            weight_lb / 1000
+          ).toFixed(2)} ton`}</Text>
         </Text>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     justifyContent: 'flex-start',
     alignItems: 'center',
+    backgroundColor: '#f0f0f0', // Adjust background color as needed
+    padding: 20,
+    marginBottom: 20,
   },
   heading: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
     color: 'green',
-    shadowColor: 'lightgreen',
     textAlign: 'center',
   },
   card: {
@@ -176,7 +363,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.2,
     shadowRadius: 3,
     padding: 20,
-    width: '80%', // Adjust the width as needed
+    width: '100%', // Adjust the width as needed
     marginTop: 10,
   },
   text: {
@@ -188,6 +375,33 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     color: 'red',
+  },
+  inputContainer: {
+    width: '100%',
+    marginBottom: 15,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 5,
+    fontWeight: 'bold', // Make labels bold
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    marginBottom: 10,
+    width: '100%',
+    backgroundColor: '#ffffff', // White background for inputs
+  },
+  column: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    textAlign: 'right',
+    fontSize: 16, // Adjust font size as needed
+    paddingRight: 5, // Add padding to separate the columns
   },
 });
 export default Details;
